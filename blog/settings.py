d816a04e7e480406a24f514dd2a61193263ed7e6
javad <javadkhoshnood194@gmail.com>
2024-04-18 09:14:17 +0330

    add acounts app

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    #my apps
+    'home',
+    'acounts'
 ]
 
 MIDDLEWARE = [
